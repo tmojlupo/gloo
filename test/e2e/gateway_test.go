20d99af7c5ec8df2e979d978822c5c4d8d71e9eb
Ashley <ashley.wang@solo.io>
2020-04-27 16:22:49 -0400

    Increase more wait timers

--- test/e2e/gateway_test.go
+++ test/e2e/gateway_test.go
@@ -242,7 +242,7 @@ var _ = Describe("Gateway", func() {
 						return 503
 					}
 					return response.StatusCode
-				}, 20*time.Second, 500*time.Millisecond).Should(Equal(503))
+				}, 30*time.Second, 500*time.Millisecond).Should(Equal(503))
 			})
 
 			It("should not match requests that contain a header that is excluded from match", func() {
